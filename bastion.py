f9c5ce3d62cacb904e4536d4271ca5e65b90d43a
Minh Hoang TO <hoang281283@gmail.com>
2016-08-20 16:31:18 +0700

    Update the code

--- bastion.py
+++ bastion.py
@@ -36,9 +36,9 @@ def open_ssh_in_tmux_and_record(host):
 
     now = datetime.datetime.now()
     log_dst = "../asciinema_logs/%s-%s-%s-%s-%s-%s_%s_%s.log" % (
-    host.user, host.domain, now.year, now.month, now.day, now.hour, now.minute, now.second)
+        host.user, host.domain, now.year, now.month, now.day, now.hour, now.minute, now.second)
 
-    ascii_cmd = "asciinema rec -c '%s' %s" % (host.connect_title(), log_dst)
+    ascii_cmd = "asciinema rec -c '%s' %s" % (host.ssh_command(), log_dst)
 
     cmd = "tmux new-window -n %s \"%s ;read\"" % (host.connect_title(), ascii_cmd)
     execute_cmd(cmd)
@@ -81,20 +81,6 @@ class Picker(object):
     def move_up(self):
         self.selected_index = (self.selected_index - 1) % self.total
 
-    def display_hosts(self, screen):
-        #screen.addstr(2, 4, 'Hosts:')
-        for i in range(len(self.hosts)):
-            host = self.hosts[i]
-            if i == self.selected_index:
-                screen.addstr(i + 4, 4, host.content() , curses.A_STANDOUT)
-            else:
-                screen.addstr(i + 4, 4, host.content())
-
-    def redraw(self, screen):
-        screen.clear()
-        self.display_hosts(screen)
-        screen.refresh()
-
 
 class SSHConnectParam(object):
     def __init__(self, user, domain, port, category, desc):
@@ -114,46 +100,124 @@ class SSHConnectParam(object):
         return '%s  |  %s | %s ' % (self.domain, self.category, self.desc)
 
 
+class Screen(object):
+    def __init__(self, window):
+        self.window = window
+        self.search_mode = False
+
+    def refresh(self):
+        self.window.refresh()
+
+    def clear(self):
+        self.window.clear()
+
+    def getch(self):
+        return self.window.getch()
+
+    def enter_search_mode(self):
+        self.search_mode = True
+
+    def exit_search_mode(self):
+        self.search_mode = False
+
+    def search_mode(self):
+        return self.search_mode
+
+    def get_width(self):
+        return self.window.getmaxyx()[1]
+
+    def get_height(self):
+        return self.window.getmaxyx()[0]
+
+    def display_header(self):
+        txt = 'Hosts'
+        if self.search_mode:
+            txt = 'Matched Hosts'
+
+        self.window.addstr(1, 4, txt)
+        self.window.hline(2, 1, '#', self.get_width() - 2)
+
+    def display_hosts(self, hosts, sidx):
+        for i in range(len(hosts)):
+            host = hosts[i]
+            if i == sidx:
+                self.window.addstr(i + 4, 4, host.content(), curses.A_STANDOUT)
+            else:
+                self.window.addstr(i + 4, 4, host.content())
+
+    def display_search_box(self):
+        if not self.search_mode:
+            return
+        txt = '[SEARCH] type something to search | [ENTER] run | [ESC] quit'
+        self.window.addstr(self.get_height() - 2, 0, txt)
+
+    def redraw(self, hosts, sidx):
+        self.clear()
+        self.display_header()
+        self.display_hosts(hosts, sidx)
+        self.display_search_box()
+        self.refresh()
+
+
 class Bastion(object):
     def __init__(self, picker, screen):
+        assert isinstance(picker, Picker)
+        assert isinstance(screen, Screen)
+
         self.picker = picker
         self.screen = screen
 
     def start(self):
-        picker.display_hosts(screen)
+        self.screen.display_header()
+        self.screen.display_hosts(self.picker.all_hosts(), self.picker.selected_index)
+
         self.start_event_loop()
 
     def start_event_loop(self):
         while True:
             key = self.screen.getch()
 
-            if key == 113:  # Quit q
-                break
+            if self.screen.search_mode:
+                self.handle_event_in_search_mode(key)
+            else:
+                if key == 113:  # Quit q
+                    break
 
-            elif key == curses.KEY_UP:
-                self.picker.move_up()
-                picker.redraw(self.screen)
+                elif key == curses.KEY_UP:
+                    self.picker.move_up()
+                    self.screen.redraw(self.picker.all_hosts(), self.picker.selected_index)
 
-            elif key == curses.KEY_DOWN:
-                self.picker.move_down()
-                picker.redraw(self.screen)
+                elif key == curses.KEY_DOWN:
+                    self.picker.move_down()
+                    self.screen.redraw(self.picker.all_hosts(), self.picker.selected_index)
 
-            elif key == 10:  # Press ENTER
-                host = picker.current()
-                open_ssh_in_tmux_and_record(host)
+                elif key == 10:  # Press ENTER
+                    host = picker.current()
+                    open_ssh_in_tmux_and_record(host)
+
+                elif key == 47:  # Press '/'
+                    self.screen.enter_search_mode()
+                    self.screen.redraw(self.picker.all_hosts(), self.picker.selected_index)
 
         curses.endwin()
         kill_tmux_session('bastion')
 
+    def handle_event_in_search_mode(self, key):
+        if key == 27:  # Press 'ESC'
+            self.screen.exit_search_mode()
+            self.screen.redraw(self.picker.all_hosts(), self.picker.selected_index)
+
 
 if __name__ == '__main__':
-    screen = curses.initscr()
-    screen.border(0)
-    picker = Picker()
-    picker.load_config()
-
+    window = curses.initscr()
+    window.border(0)
     curses.noecho()
     curses.cbreak()
-    screen.keypad(1)
+    window.keypad(1)
+
+    screen = Screen(window)
+
+    picker = Picker()
+    picker.load_config()
 
     Bastion(picker, screen).start()
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ default_user = 'root'
 default_port = 22
 
 hosts = [
-    {'user': 'toto', 'domain': '192.168.10.1', 'category': 'API Servers', 'desc': 'Blah blah 1'},
+    {'user': 'toto', 'domain': '192.168.10.1', 'port': 2222, 'category': 'API Servers', 'desc': 'Blah blah 1'},
     {'user': 'tutu', 'domain': '192.168.10.2', 'category': 'API Servers', 'desc': 'Blah blah 2'},
     {'user': 'toto', 'domain': '192.168.10.3', 'category': 'API Servers', 'desc': 'Blah blah 3'},
     {'user': 'titi', 'domain': '192.168.10.4', 'category': 'Web Servers', 'desc': 'Blah blah 4'}
